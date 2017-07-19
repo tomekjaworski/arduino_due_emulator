52c423f2e2f8e490418438c9df6337b651420103
cagataytanyildiz <cagataytanyildiz@protonmail.com>
2017-07-19 10:31:16 +0200

    We changed project structure and variable types.

--- uVisionTest0/test0/main.c
+++ uVisionTest0/test0/main.c
@@ -8,15 +8,6 @@ int main(void)
 	while(1){
 		//Let's write some code
 		
-		volatile int x = 0;
-		x = 1;
-		
-		volatile int y = 1;
-		
-		y = y + 1;
-		
-		volatile int z = 0;
-		
-		z = x + y;
+		volatile int Y = 7;
 	}
 }
