a69ced1e6306838f16c0ab72eab52b71859284a4
Tomek Jaworski <tjaworski@iis.p.lodz.pl>
2017-07-21 15:55:15 +0200

    Missing entry point record type

--- TextLoader/IntelHexParser.cpp
+++ TextLoader/IntelHexParser.cpp
@@ -114,13 +114,22 @@ int32_t IntelHexParser::Load(IMemoryLoaderConnector & connector)
 				continue;
 			}
 
-			// entry point
+			// entry point in line space
 			if (record_type == 5)
 			{
 				// for future use
 				uint32_t ep_addr = data[4] << 24 | data[5] << 16 | data[6] << 8 | data[7];
 				continue;
 			}
+
+			// entry point in line space
+			if (record_type == 3)
+			{
+				// for future use
+				uint32_t ep_addr = data[4] << 24 | data[5] << 16 | data[6] << 8 | data[7];
+				continue;
+			}
+
 
 			throw IntelHexParserException(b::str(b::format("Unknown record type: %02x") % record_type));
 
