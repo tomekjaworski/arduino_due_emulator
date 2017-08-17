3fe96962601510b9c8f1f9f04aec59787172be76
cagataytanyildiz <cagataytanyildiz@protonmail.com>
2017-08-17 11:38:28 +0200

    Changing dummy memory to virtual memory.

--- mm_loader/mm_loader.cpp
+++ mm_loader/mm_loader.cpp
@@ -10,11 +10,13 @@
 
 #include "..\MemoryMap\DummyMemoryMap.hpp"
 #include "..\TextLoader\TextLoader.hpp"
+#include "..\MemoryMap\VirtualMemoryMap.h"
 
 template <typename P>
 void test(const char* file_name, bool expected_result)
 {
-	DummyMemoryMap dmm;
+	//DummyMemoryMap dmm;
+	VirtualMemoryMap memory;
 	P p(file_name);
 
 	std::cout << "Test for " << file_name << ": ";
@@ -23,7 +25,7 @@ void test(const char* file_name, bool expected_result)
 	std::string msg = "";
 	int bytes = -1;
 	try {
-		bytes = p.Load(dmm);
+		bytes = p.Load(memory);
 	}
 	catch (const std::exception& ex)
 	{
@@ -46,6 +48,7 @@ void test(const char* file_name, bool expected_result)
 		std::cout << "; Byte count=" << bytes;
 
 	std::cout << std::endl;
+	memory.DumpMemory();
 }
 
 int main()
@@ -53,7 +56,7 @@ int main()
 	//test<ELFParser>("test_axf\\test0_ok.axf", 1);
 
 	test<IntelHexParser>("test_hex\\GccApplication2.hex", 1);
-	test<IntelHexParser>("test_hex\\test1_ok.hex", 1);
+	/*test<IntelHexParser>("test_hex\\test1_ok.hex", 1);
 	test<IntelHexParser>("test_hex\\test2_ok.hex", 1);
 	test<IntelHexParser>("test_hex\\test3_ok.hex", 1);
 	test<IntelHexParser>("test_hex\\test4_ok.hex", 1);
@@ -63,6 +66,8 @@ int main()
 	test<IntelHexParser>("test_hex\\test8_ok.hex", 1);
 	test<IntelHexParser>("test_hex\\test9_ok.hex", 1);
 	test<IntelHexParser>("test_hex\\test10_ok.hex", 1);
+	*/
 	
+	while(1){}
 	return 0;
 }
