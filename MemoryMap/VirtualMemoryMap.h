4bcf16a929cbcc1f5173bfac98181ba4364f78c1
cagataytanyildiz <cagataytanyildiz@protonmail.com>
2017-08-17 11:39:39 +0200

    Adding dump functions to virtual memory class

--- MemoryMap/VirtualMemoryMap.h
+++ MemoryMap/VirtualMemoryMap.h
@@ -29,6 +29,41 @@ public:
 
 	virtual bool Write(uint32_t address, uint32_t value) override;
 
+	void DumpMemory() {
+		int flash_size = 256 * 1024 / 4;
+		int rom_size = 128 * 1024 / 4;
+		int sram_size = 64 * 1024 / 4;
+		int check_content = 16;
+
+		printf("FLASH 0\n");
+		printf("---------\n");
+		for (int i = 0; i < (flash_size / 100); i++) {
+			printf("%2x ", flash_0[i]);
+			if (i % 15 == 0 && i!=0) {
+				printf("\n");
+			}
+		}
+		printf("FLASH 1\n");
+		printf("---------\n");
+		for (int i = 0; i < check_content; i++) {
+			printf("%x\n", flash_1[i]);
+		}
+		printf("ROM\n");
+		printf("---------\n");
+		for (int i = 0; i < check_content; i++) {
+			printf("%x\n", rom[i]);
+		}
+		printf("SRAM 0\n");
+		printf("---------\n");
+		for (int i = 0; i < check_content; i++) {
+			printf("%x\n", sram_0[i]);
+		}
+		printf("SRAM 1\n");
+		printf("---------\n");
+		for (int i = 0; i < check_content; i++) {
+			printf("%x\n", sram_1[i]);
+		}
+	}
 };
 
 #endif // _DUMMY_MEMORY_MAP_HPP_
